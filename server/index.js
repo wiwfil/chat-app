785b35bf03fe6b95da19a7eec8da6badc5fc3888
Muhammed Kurt <kurtmu17@itu.edu.tr>
2022-10-06 21:27:29 +0300

    update

--- server/index.js
+++ server/index.js
@@ -49,8 +49,8 @@ const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "*",
-    credentials: false,
-    transports: ["websocket"]
+    credentials: true,
+    transports: ['websocket', 'polling', 'flashsocket'] 
   },
 });
 io.origins('*:*')
